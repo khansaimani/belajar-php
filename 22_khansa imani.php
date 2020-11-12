ef5d573d506df25ba7e05f2566f3a7df67287b1a
khansaimani <khansaimani1919@gmail.com>
2020-11-12 22:47:32 +0700

    first commit

---
+++ 22_khansa imani.php
@@ -0,0 +1,16 @@
+<?php
+//membuat array
+$siswa = [
+    "Satria",
+    "Arsyi",
+    "Dhika",
+    "Evan",
+    "Febian",
+    "Irfan"
+];
+
+//menampilkan output dengan print_r
+echo "<pre>";
+print_r($siswa);
+echo "</pre>";
+?>
